4031151d184902d963eafd8e050695dc05db1792
Nivetha2308 <nivetha23mega@gmail.com>
2024-07-06 01:41:49 +0530

    first commit

---
+++ Ch2_pgm_03.js
@@ -0,0 +1,26 @@
+// Using a variable
+
+var score;
+score = 100;
+console.log(score);
+var bat;
+bat = 150;
+console.log(bat);
+var player;
+player = 160;
+console.log(player);
+
+
+
+/* Further Adventures
+ *
+ * 1) Change the value assigned to score.
+ *    Run the program again.
+ *
+ * 2) Declare a second variable, maybe score2.
+ *
+ * 3) Assign your variable a value.
+ *
+ * 4) Use console.log to display your variable on the console.
+ *
+ */
